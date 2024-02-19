341e97697b3d41d8db7173e45fc2d0c56858fe4d
redhottensors <159090586+redhottensors@users.noreply.github.com>
2024-02-18 17:55:33 -0700

    add metadata for ComfyUI-Manager

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
+"""
+@author: RedHotTensors
+@title: ComfyUI-Prediction
+@nickname: ComfyUI-Prediction
+@description: Fully customizable Classifer Free Guidance for ComfyUI
+"""
+
 from .nodes.nodes_pred import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
